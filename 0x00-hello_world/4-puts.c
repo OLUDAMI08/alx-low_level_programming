66b92cb1c41bdb8806a4640d9d465ffcd4312b4f
lawal toheeb damilare <lawaltd088@gmail.com>
2022-09-08 19:18:51 +0000

    My modified commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success
+ */
 int main(void)
-{char str[170] = "Programming is like building a multilingual puzzle";
- puts(str);
- return 0;
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
