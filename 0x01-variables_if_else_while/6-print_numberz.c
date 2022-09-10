3941ec0825cb48bf6dd379ac4e8be31ab2511119
lawal toheeb damilare <lawaltd088@gmail.com>
2022-09-10 13:01:48 +0000

    My first commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * author: oludami08
+ * date: 10th sept 2022
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		putchar('\n');
+		i++;
+	}
+	return (0);
+}
