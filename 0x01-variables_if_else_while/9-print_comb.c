6f2f59e2335d19faf5a97ea9fa1e3cd4b269c787
lawal toheeb damilare <lawaltd088@gmail.com>
2022-09-11 09:06:58 +0000

    My first commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: oludami08
+ * date: 11 sept 2022
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+		if (num != '9')
+		{
+
+			putchar(num);
+			putchar(',');
+			putchar(' ');
+
+		}
+		else
+			putchar(num);
+}
