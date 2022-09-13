d4d8bb78fbc1f378373ff60512cc87d9d29dca36
lawal toheeb damilare <lawaltd088@gmail.com>
2022-09-13 23:12:28 +0000

    My first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * author: oludami08
+ * date: 12 sept 2022
+ * Return: always 0 (0)
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = a; alp <= 'z'; alp++)
+		putchar(alp);
+	return (0);
+}
