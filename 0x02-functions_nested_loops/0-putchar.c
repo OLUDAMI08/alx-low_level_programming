07877f9d28735f1e4c297770bb4cb471095c46b7
lawal toheeb damilare <lawaltd088@gmail.com>
2022-09-13 19:46:53 +0000

    My first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: oludami08
+ * date: 12 sept 2022
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char s[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		putchar(s[i]);
+	return (0);
+}
