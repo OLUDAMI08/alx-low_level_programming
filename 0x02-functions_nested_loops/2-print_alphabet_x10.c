dc3290cd25ee31b88c5140c30421ecc6f0b4bfaa
lawal toheeb damilare <lawaltd088@gmail.com>
2022-09-14 10:07:40 +0000

    My first commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that print 10 times the alphabet
+ * author: oludami08
+ * date: 13 sept 2022
+ * Return: 0
+ */
+
+void print_alphabet_x10(void);
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
