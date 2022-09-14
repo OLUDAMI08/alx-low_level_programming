b84ab1627b0100339e5b8b1f8dc81bc2e904adef
lawal toheeb damilare <lawaltd088@gmail.com>
2022-09-14 16:22:33 +0000

    My first commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,16 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = (n % 10);
-	return (last_digit);
+	for (n < 0)
+	{
+		last_digit = -1 * (n % 10);
+		_putchar(last_digitn + '0');
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = (n % 10);
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
 }
