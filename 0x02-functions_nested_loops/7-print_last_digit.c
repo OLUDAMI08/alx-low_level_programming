65d46613471b666b31d1711722e67b720ceb2048
lawal toheeb damilare <lawaltd088@gmail.com>
2022-09-14 16:11:56 +0000

    My first commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - the last digit
+ * @n: integer to check
+ * author: oludami08
+ * date 13 sept 2022
+ * Return: 0 always for sucess
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	last_digit = (n % 10);
+	return (last_digit);
+}
