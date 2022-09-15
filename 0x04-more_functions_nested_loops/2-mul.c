4c1e68b554db4a45f3cf37ef2260f1cebfc8b8c4
lawal toheeb damilare <lawaltd088@gmail.com>
2022-09-15 20:37:03 +0000

    My first commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul- multiply two numbers
+ * @a: checking the first number
+ * @b: checking the second number
+ * Return: mult
+ */
+
+int mul(int a, int b)
+{
+
+	int mult;
+
+	mult = a * b;
+
+	return (mult);
+}
