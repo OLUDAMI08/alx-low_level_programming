90cab0cbb8c76ae17d0eb8d97582b5c7d4ca0601
lawal toheeb damilare <lawaltd088@gmail.com>
2022-09-16 14:19:18 +0000

    My first commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,13 +3,17 @@
 /**
  * _isdigit - check for a digit (0 through 9)
  * @c: character to check
- * Return: 1 if c is a digit, 0 otherwise
+ * Return: 0 or 1
  */
 
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 58)
+	if (c >= 0 && c <= 9)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
