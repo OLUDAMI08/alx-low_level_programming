3dea436f5ce4bc2720a97800c8ad6f42f5c3e579
lawal toheeb damilare <lawaltd088@gmail.com>
2022-10-25 19:52:45 +0000

    print and return number of nodes in the linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,23 +1,19 @@
 #include "lists.h"
-
 /**
- * print_listint - print all the element of the linked list
- *
+ * print_listint - prints all the elements of a linked list
  * @h: pointer to the 1st node of the linked list
- *
  * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
-
+	size_t nodenum = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		nodes++;
+		nodenum++;
 		h = h->next;
 	}
-	return (nodes);
+
+	return (nodenum);
 }
