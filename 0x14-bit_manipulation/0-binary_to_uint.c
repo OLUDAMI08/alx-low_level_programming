03169b6e7d269b91f19ef57f4a18b5fc0f93f8dc
lawal toheeb damilare <lawaltd088@gmail.com>
2022-10-27 20:57:22 +0000

    convert binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,34 @@
-#include "main.h>
-
+#include "main.h"
 /**
   * binary_to_uint - converts a binary number to an unsigned int
   * @b: binary number
   * Return: the converted number or 0 if it fails
   */
-
 unsigned int binary_to_uint(const char *b)
 {
-	int i, j, strlen = 0;
-    int dec = 1;
-    int total = 0;
+	unsigned int uint, place;
+	int n;
 
-    if (!b)
-	    return (0);
+	if (!b)
+		return (0);
 
-    for (i = 0; b[i]; i++)
-    {
-        strlen++;
-    }
-
-    for (j = strlen - 1; j >= 0; j--)
-    {
-        if (b[j] == '1') 
-        total = total + dec;
-        dec = dec * 2;
-    }
-    return (total);
-    
+	for (n = 0; b[n]; n++)
+		;
+	n--;
+	for (place = 1, uint = 0; n >= 0; n--)
+	{
+		if (b[n] == '0')
+		{
+			place *= 2;
+			continue;
+		}
+		else if (b[n] == '1')
+		{
+			uint += place;
+			place *= 2;
+			continue;
+		}
+		return (0);
+	}
+	return (uint);
 }
-
