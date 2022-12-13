bc609f25b0875a3c5474e3b5c30d0abc08321d26
lawal toheeb damilare <lawaltd088@gmail.com>
2022-12-13 09:12:42 +0000

    function that add the element of a list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - function to get sum of all values of a list
+ *
+ * @head: pointer pointing to the head node
+ *
+ * Return:  the sum of all the data (n) of a dlistint_t linked list.
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (!(head))
+		return (0);
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
