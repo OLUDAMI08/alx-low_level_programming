ab32f32c6c7a4058979ac8e92824b702c4cc5bea
Lawal Damilare Toheeb <lawaltd088@gmail.com>
2023-04-09 16:19:50 +0100

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
-	unsigned long int i;
+	unsigned int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
