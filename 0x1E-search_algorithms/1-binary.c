f557fc9d6f5b2be0dd84b2dcfd1a9f396e3a9f38
Lawal Damilare Toheeb <lawaltd088@gmail.com>
2023-04-13 16:32:37 +0100

    update

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,63 +1,45 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for a value in a sorted array of integers using
- * the Binary search algorithm
- *
- * @array: pointer to the first element of the array to search in
+ * binary_search - searches for a value in an integer array using a binary
+ * search algorithm, not guaranteed to return lowest index if `value` appears
+ * twice in `array`
+ * @array: pointer to first element of array to seach
  * @size: number of elements in array
  * @value: value to search for
  *
- * Return: index where value is located, or -1 if value is not present in
- * array or if array is NULL
+ * Return: index containing `value`, or -1 if `value` not found or
+ * `array` is NULL
  */
 
-void print_array(int *array, size_t low, size_t high)
-{
-	char *separator = NULL;
-
-	printf("Searching in array: ");
-	for (; low <= high; low++)
-	{
-		if (separator)
-			printf("%s%d", separator, array[low]);
-		else
-			printf("%d", array[low]);
-		separator = ", ";
-	}
-	printf("\n");
-}
-
-/**
- * binary_search - searches for a value in an array of integers using the
- *		binary search algorithm
- * @array: a pointer to the first element of the array to search in
- * @size: the number of elements in array
- * @value: the value to search for
- *
- * Return: the first index where value is located, else, -1
- */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low = 0, mid, high = size - 1;
+	int low, mid, high;
+	int x;
 
 	if (array == NULL)
+	{
 		return (-1);
+	}
 
-	print_array(array, low, high);
-	while (low != high)
+	low = 0;
+	high = size - 1;
+
+	while (low <= high)
 	{
 		mid = (low + high) / 2;
 
-		if (array[mid] == value)
-			return (mid);
-		else if (array[mid] < value)
+		printf("Searching in array: ");
+		for (x = low; x <= high; x++)
+			printf("%i%s", array[x], x == high ? "\n" : ", ");
+
+		if (array[mid] < value)
 			low = mid + 1;
-		else
+		else if (array[mid] > value)
 			high = mid - 1;
-		print_array(array, low, high);
+		else
+			return (mid);
 	}
-	if (array[low] == value)
-		return (low);
+
 	return (-1);
 }
